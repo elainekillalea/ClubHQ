5979eda9a897bca3543ed177c714560b5ab58519
elaineKillalea <g00370088@atu.ie>
2022-11-02 11:13:05 +0000

    Forgot 2 folders when last commiting

---
+++ components/layout/Layout.js
@@ -0,0 +1,13 @@
+import MainNav from './MainNav';
+import classes from './Layout.module.css';
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
